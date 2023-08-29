f2c15f3bddd3c920843b0ce740782629f5b3e5e0
abdoali796 <111509346+abdoali796@users.noreply.github.com>
2023-08-29 20:27:18 +0300

    Create timer.kt

---
+++ timer.kt
@@ -0,0 +1,149 @@
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.CountDownTimer
+import android.os.SystemClock
+import androidx.core.app.AlarmManagerCompat
+import dagger.hilt.android.internal.Contexts.getApplication
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+class Timer @Inject constructor(@ApplicationContext context: Context) {
+
+    private var job: Job? = null
+    private val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+    private val REQUEST_CODE = 0
+    private val TRIGGER_TIME = "TRIGGER_AT"
+
+    private val minute: Long = 60_000L
+    private val second: Long = 1_000L
+
+
+    private val notifyIntent = Intent(context , AlarmReceiver::class.java)
+    private var prefs =
+        context.getSharedPreferences("com.abdoali.timer" , Context.MODE_PRIVATE)
+
+        
+    private val _timeSelection = MutableStateFlow<Int>(5)
+    val timeSelection: MutableStateFlow<Int>
+        get() = _timeSelection
+
+    private lateinit var notifyPendingIntent: PendingIntent
+//to know timer on or not
+    private var _alarmOn = MutableStateFlow<Boolean>(false)
+    val isAlarmOn: StateFlow<Boolean>
+        get() = _alarmOn
+
+// to elapsedTime
+    private val _elapsedTime = MutableStateFlow<Long>(0L)
+    val elapsedTime: StateFlow<Long>
+        get() = _elapsedTime
+
+    private lateinit var timer: CountDownTimer
+
+    init {
+        job = Job()
+        _alarmOn.value = PendingIntent.getBroadcast(
+            getApplication(context) ,
+            REQUEST_CODE ,
+            notifyIntent ,
+            PendingIntent.FLAG_NO_CREATE or PendingIntent.FLAG_IMMUTABLE
+        ) != null
+
+        notifyPendingIntent = PendingIntent.getBroadcast(
+            getApplication(context) ,
+            REQUEST_CODE ,
+            notifyIntent ,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        if (_alarmOn.value !!) {
+            createTimer()
+        }
+
+    }
+
+    private fun createTimer() {
+        job.run {
+
+            val triggerTime = loadTime()
+            timer = object : CountDownTimer(triggerTime , second) {
+                override fun onTick(millisUntilFinished: Long) {
+                    _elapsedTime.tryEmit(triggerTime - SystemClock.elapsedRealtime())
+                    if (_elapsedTime.value <= 0) {
+                        resetTimer()
+                    }
+                }
+
+                override fun onFinish() {
+                    resetTimer()
+                }
+            }
+            timer.start()
+        }
+    }
+
+    private fun resetTimer() {
+        timer.cancel()
+
+        _elapsedTime.tryEmit(0)
+        _alarmOn.value = false
+    }
+
+//startTimer
+    fun setAlarm(isChecked: Boolean) {
+        when (isChecked) {
+            true -> timeSelection.value?.let { startTimer(it) }
+            false -> resetTimer()
+        }
+    }
+
+    //setTimeSelected
+    fun setTimeSelected(timerLengthSelection: Int) {
+        _timeSelection.value = timerLengthSelection
+    }
+
+    private fun startTimer(timerLengthSelection: Int) {
+        _alarmOn.value?.let {
+            if (! it) {
+                _alarmOn.value = true
+                val selectedInterval = when (timerLengthSelection) {
+                    0 -> second * 10 //For testing only
+                    else -> timerLengthSelection * minute
+                }
+                val triggerTime = SystemClock.elapsedRealtime() + selectedInterval
+
+
+
+                AlarmManagerCompat.setExactAndAllowWhileIdle(
+                    alarmManager ,
+                    AlarmManager.ELAPSED_REALTIME_WAKEUP ,
+                    triggerTime ,
+                    notifyPendingIntent
+                )
+
+                job.run {
+                    saveTime(triggerTime)
+                }
+            }
+        }
+        createTimer()
+    }
+
+    private fun saveTime(triggerTime: Long) =
+        job.run {
+            prefs.edit().putLong(TRIGGER_TIME , triggerTime).apply()
+        }
+
+    private fun loadTime(): Long =
+        job.run {
+            prefs.getLong(TRIGGER_TIME , 0)
+        }
+
+}
+
